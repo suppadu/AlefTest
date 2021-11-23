f6bedb5861737db81a69f634f6c6313db3fdf411
konstantinov_d <konstantinov.d@orionnet.ru>
2021-11-23 15:56:26 +0800

    Initial Commit

---
+++ AlefTest/AlefTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlefTestApp.swift
+//  AlefTest
+//
+//  Created by Дмитрий Константинов on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AlefTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlefTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlefTest
+//
+//  Created by Дмитрий Константинов on 23.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
